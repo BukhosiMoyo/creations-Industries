e14c4a48d9f500d2b31205655086c59f00b41db9
BukhosiMoyo <khosicodes@gmail.com>
2026-02-12 18:22:34 +0200

    Finalize login page and auth components

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,7 +10,7 @@ export const authOptions: NextAuthOptions = {
         strategy: "jwt",
     },
     pages: {
-        signIn: "/auth/login",
+        signIn: "/login",
     },
     providers: [
         CredentialsProvider({
